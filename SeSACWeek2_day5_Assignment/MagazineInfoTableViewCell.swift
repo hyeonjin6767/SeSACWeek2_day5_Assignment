9f1200a7c8baf2035c938fd8afde8895ecd83cc6
hyeonjin6767 <parkhj067670@gmail.com>
2025-07-12 15:32:56 +0900

    "MagazineInfo"TableViewController, TableViewCell 생성 및 Cell AutoLayout 설정

---
+++ SeSACWeek2_day5_Assignment/MagazineInfoTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MagazineInfoTableViewCell.swift
+//  SeSACWeek2_day5_Assignment
+//
+//  Created by 박현진 on 7/12/25.
+//
+
+import UIKit
+
+class MagazineInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
---
+++ SeSACWeek2_day5_Assignment/MagazineInfoTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MagazineInfoTableViewController.swift
+//  SeSACWeek2_day5_Assignment
+//
+//  Created by 박현진 on 7/12/25.
+//
+
+import UIKit
+
+class MagazineInfoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+
+    }
+
+
+}
