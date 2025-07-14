9a5ed5fa62c153da5b34bc1ec6d97d4ed5dbb736
hyeonjin6767 <parkhj067670@gmail.com>
2025-07-14 20:09:54 +0900

    유동적인 값이 필요한 디자인 함수(configureSetting)를 만들어 정리

--- SeSACWeek2_day5_Assignment/ForXIBMagazineTableViewController.swift
+++ SeSACWeek2_day5_Assignment/ForXIBMagazineTableViewController.swift
@@ -11,12 +11,10 @@ class ForXIBMagazineTableViewController: UITableViewController {
 
     let magazineInfo = MagazineInfo()
     
-    let format = DateFormatter() //위치 변경
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //XIB로 변경
+        //XIB로 변경 //셀 등록
         let xib = UINib(nibName: "MagazineTableViewCell", bundle: nil)
         tableView.register(xib, forCellReuseIdentifier: "MagazineTableViewCell")
 
@@ -29,29 +27,14 @@ class ForXIBMagazineTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineTableViewCell", for: indexPath) as! MagazineTableViewCell
-        cell.magazineTitleLabel.text = magazineInfo.magazine[indexPath.row].title
+        let rowInfo = magazineInfo.magazine[indexPath.row]
+        cell.configureSetting(row: rowInfo)
         
-        let url = URL(string: magazineInfo.magazine[indexPath.row].photo_image)!
-        cell.magazinePhotoImageView.kf.setImage(with: url)
-        cell.magazineSubtitleLabel.text = magazineInfo.magazine[indexPath.row].subtitle
-        
-        //https://formestory.tistory.com/m/6
-        let dateString = magazineInfo.magazine[indexPath.row].date
-        //let format = DateFormatter() //위로
-        format.dateFormat = "yyMMdd"
-        let convertDate = format.date(from: dateString)
-        let magazineDateForm = DateFormatter()
-        magazineDateForm.dateFormat = "yy년 MM월 dd일"
-        let convertString = magazineDateForm.string(from: convertDate!)
-        cell.magazineDateLabel.text = convertString
-       
         return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 500
+        //return UITableView.automaticDimension
     }
-
-    
-
 }
--- SeSACWeek2_day5_Assignment/MagazineTableViewCell.swift
+++ SeSACWeek2_day5_Assignment/MagazineTableViewCell.swift
@@ -10,21 +10,34 @@ import UIKit
 class MagazineTableViewCell: UITableViewCell {
 
     @IBOutlet var magazinePhotoImageView: UIImageView!
-    
     @IBOutlet var magazineTitleLabel: UILabel!
-    
     @IBOutlet var magazineSubtitleLabel: UILabel!
-    
     @IBOutlet var magazineDateLabel: UILabel!
     
-    override func awakeFromNib() {
+    let format = DateFormatter() //위치 변경
+    
+    override func awakeFromNib() { //고정값이 있는 디자인
         super.awakeFromNib()
         
         magazinePhotoImageView.layer.cornerRadius = 10
         magazineTitleLabel.font = .boldSystemFont(ofSize: 20)
-       
     }
-
     
-    
+    func configureSetting(row: Magazine) { //변동값이 필요한 디자인
+        
+        magazineTitleLabel.text = row.title
+        
+        let url = URL(string: row.photo_image)!
+        magazinePhotoImageView.kf.setImage(with: url)
+        magazineSubtitleLabel.text = row.subtitle
+        
+        let dateString = row.date
+        //let format = DateFormatter() //위로
+        format.dateFormat = "yyMMdd"
+        let convertDate = format.date(from: dateString)
+        let magazineDateForm = DateFormatter()
+        magazineDateForm.dateFormat = "yy년 MM월 dd일"
+        let convertString = magazineDateForm.string(from: convertDate!)
+        magazineDateLabel.text = convertString
+    }
 }
