6c49cd75392f641dfb66d8e896c2f7033d945a23
hyeonjin6767 <parkhj067670@gmail.com>
2025-07-15 17:31:09 +0900

    push화면전환 최대한 비슷하게 만들기(관광지 화면)

--- SeSACWeek2_day5_Assignment/TravelShowViewController.swift
+++ SeSACWeek2_day5_Assignment/TravelShowViewController.swift
@@ -20,9 +20,18 @@ class TravelShowViewController: UIViewController {
         //임의의 데이터 넣어주기
         let url = URL(string: travelInfo.travel[5].travel_image)!
             travelImageView.kf.setImage(with: url)
+        travelImageView.layer.cornerRadius = 10
+        travelImageView.contentMode = .scaleAspectFill
+        travelTitleLabel.textAlignment = .center
+        travelTitleLabel.font = .boldSystemFont(ofSize: 30)
         travelTitleLabel.text = travelInfo.travel[5].title
         travelDescriptionLabel.text = travelInfo.travel[5].description
+        travelDescriptionLabel.textAlignment = .center
+        travelDescriptionLabel.font = .boldSystemFont(ofSize: 15)
         popButton.setTitle("다른 광광지 보러가기", for: .normal)
+        popButton.setTitleColor(.white, for: .normal)
+        popButton.backgroundColor = .systemBlue
+        popButton.layer.cornerRadius = 10
     }
     
     @IBAction func popButtonClicked(_ sender: UIButton) {
