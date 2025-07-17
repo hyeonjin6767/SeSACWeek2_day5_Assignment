96374e9bccc335412e66874d4b605554ee6dc2af
hyeonjin6767 <parkhj067670@gmail.com>
2025-07-17 21:34:35 +0900

    Segmented Control를 통해 값 넘기기, ViewController + CollectionView 조합 사용해보기

---
+++ SeSACWeek2_day5_Assignment/CollectionPopularCityViewController.swift
@@ -0,0 +1,93 @@
+//
+//  CollectionPopularCityViewController.swift
+//  SeSACWeek2_day5_Assignment
+//
+//  Created by 박현진 on 7/17/25.
+//
+
+import UIKit
+
+class CollectionPopularCityViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    var cityInfo = CityInfo()
+    
+    @IBOutlet var cityCollectionView: UICollectionView!
+    @IBOutlet var citySegmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //citySegmentedControl.titleForSegment(at: 0)
+        citySegmentedControl.setTitle("모두", forSegmentAt: 0)
+        citySegmentedControl.setTitle("국내", forSegmentAt: 1)
+        citySegmentedControl.setTitle("해외", forSegmentAt: 2)
+
+        cityCollectionView.delegate = self
+        cityCollectionView.dataSource = self
+        
+        let xib = UINib(nibName: "PopularCitiesCollectionViewCell", bundle: nil)
+        cityCollectionView.register(xib, forCellWithReuseIdentifier: "PopularCitiesCollectionViewCell")
+        
+        let layout = UICollectionViewFlowLayout()
+        let deviceWidth = UIScreen.main.bounds.width
+        let cellWidth = deviceWidth - (15 * 2) - (15 * 1)
+        layout.itemSize = CGSize(width: cellWidth/2, height: cellWidth/2)
+        layout.sectionInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        layout.minimumLineSpacing = 20
+        layout.minimumInteritemSpacing = 20
+        layout.scrollDirection = .horizontal
+        cityCollectionView.collectionViewLayout = layout
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cityInfo.city.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PopularCitiesCollectionViewCell", for: indexPath) as! PopularCitiesCollectionViewCell
+        let url = URL(string: cityInfo.city[indexPath.item].city_image)!
+        cell.cityImageView.kf.setImage(with: url)
+        cell.cityNameLabel.text = cityInfo.city[indexPath.item].city_name
+        cell.cityExplainLabel.text = cityInfo.city[indexPath.item].city_explain
+        return cell
+    }
+    
+    @IBAction func citySegmentedControlChanged(_ sender: UISegmentedControl) {
+        //print(sender.selectedSegmentIndex)
+        //print(sender.largeContentTitle ?? 0)
+        //print(sender.titleForSegment(at: 0))
+        let all = CityInfo().city
+        if sender.selectedSegmentIndex == 0 {
+            cityInfo.city = all
+            cityCollectionView.reloadData()
+        } else if sender.selectedSegmentIndex == 1 {
+            var filter: [City] = []
+            for item in all {
+                if item.domestic_travel {
+                    filter.append(item)
+                }
+            }
+            cityInfo.city = filter
+            cityCollectionView.reloadData()
+        } else if sender.selectedSegmentIndex == 2 {
+            var filter: [City] = []
+            for item in all {
+                if !item.domestic_travel {
+                    filter.append(item)
+                }
+            }
+            cityInfo.city = filter
+            cityCollectionView.reloadData()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let viewcontroller = self.storyboard?.instantiateViewController(withIdentifier: "PopularCityViewController") as! PopularCityViewController
+        
+        viewcontroller.popularCitys = cityInfo.city[indexPath.item]
+        
+        present(viewcontroller, animated: true)
+    }
+
+}
--- SeSACWeek2_day5_Assignment/PopularCityViewController.swift
+++ SeSACWeek2_day5_Assignment/PopularCityViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class PopularCityViewController: UIViewController {
 
     @IBOutlet var popularCityImageView: UIImageView!
-    @IBOutlet var popualrCityNameLabel: UILabel!
+    @IBOutlet var popularCityNameLabel: UILabel!
     
     var popularCitys: City = City(city_name: "", city_english_name: "", city_explain: "", city_image: "", domestic_travel: true)
     
@@ -18,9 +18,9 @@ class PopularCityViewController: UIViewController {
         super.viewDidLoad()
         
         popularCityImageView.contentMode = .scaleAspectFill
-        popualrCityNameLabel.textAlignment = .center
+        popularCityNameLabel.textAlignment = .center
         
-        popualrCityNameLabel.text = popularCitys.city_name
+        popularCityNameLabel.text = popularCitys.city_name
         let url = URL(string: popularCitys.city_image)!
         popularCityImageView.kf.setImage(with: url)
     }
